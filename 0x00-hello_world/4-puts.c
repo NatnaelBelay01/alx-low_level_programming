08b6c566406245adabe6d4ccf78923dd4fb578ff
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-07 03:52:05 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * this is my main function's description
+ * main - this is a  description of main
+ * Return: It returns zero
  */
 int main(void)
 { 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
