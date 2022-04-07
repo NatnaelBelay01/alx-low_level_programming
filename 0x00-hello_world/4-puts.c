b332a5454c28c00e1b92e153f1cca043a80ce778
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-07 02:32:25 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void) /*this is the main function */
+int main(void) 
+/*this is the dicription for the main function */
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
