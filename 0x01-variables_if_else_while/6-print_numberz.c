50947e1a636ce75ac56190b4957aa2dc058aed7d
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 07:06:21 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -This is the discription for the main function
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
