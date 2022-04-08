fdf6a0f3d6c9eaee304f573e306ab65a7311d9dc
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 06:07:56 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -this is the description for the main function
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		if (alp != q && alp != e)
+		{
+			putchar(alp);
+			alp++;
+		}
+		else
+		{
+			alp++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
