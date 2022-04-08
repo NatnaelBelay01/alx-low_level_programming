5d1b9b5dabc6cf7c8a8848ac3f7a6ac360fe1a2c
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 06:42:20 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -This is the discription of the main function
+ * Return: returns zero
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+putchar('\n');
+return (0);
+}
