c28539849ccf7a916123510134816ab3fee3fcc4
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 05:25:19 -0700

    hot fixes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ else if (m == 0)
 }
 else
 {
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 return (0);
 }
