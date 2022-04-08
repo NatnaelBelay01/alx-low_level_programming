655790a932502001577333666b10123d8073e586
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 09:17:53 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -this is a discription of the main function
+ * Return: 0(success !)
+ */
+int main(void)
+{
+	char b_16;
+
+	for (b_16 = 48; b_16 <= 57; b_16++)
+	{
+		putchar(b_16);
+	}
+	for (b_16 = 97; b_16 <= 102; b_16++)
+	{
+		putchar(b_16);
+	}
+	putchar('\n')
+	return (0);
+}
