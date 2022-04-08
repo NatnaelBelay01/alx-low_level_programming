47fc718a69775a879713436768e0974b45a5c13f
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 14:04:50 -0700

    Advanced task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -this is the discription for main
+ * Return: 0 (a success)
+ */
+int main(void)
+{
+	int num1 = 48, num2 = 49, num3 = 50;
+
+	while (num1 < 56)
+	{
+		while (num2 < 57)
+		{
+			while (num3 <= 57)
+			{
+				if (num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					putchar(' ');
+				}
+				num3++;
+			}
+			num3 = 48;
+			num2++;
+		}
+		num2 = 48;
+		num1++;
+	}
+	return (0);
+}
