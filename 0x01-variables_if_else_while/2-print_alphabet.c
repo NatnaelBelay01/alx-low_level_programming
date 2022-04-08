397cf8b0f60c64b3d6b0084d82778887c37a13e5
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-08 01:44:29 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -this is the discription for the main function
+ * Return: returns 0(success !)
+ */
+int main(void)
+{
+char a = 'a';
+
+while (a <= z)
+{
+	putchar(a);
+	putchar("\n");
+	a++;
+}
+return (0);
+}
