e4005d113d24cc742790c382bb21e01e884a7452
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-12 05:05:51 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case character
+ * Return: 1 if true 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
