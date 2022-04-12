cf4574476598ce6d8639b84ca7af6dafd38a9182
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-12 05:58:24 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number to be checked
+ * Return: returns the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
