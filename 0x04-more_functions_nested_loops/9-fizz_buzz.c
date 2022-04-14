6b80989fc94ede22cad1a7783ca89023cf286e2f
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-14 06:47:48 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,6 +9,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i > 1)
+			putchar(' ');
 		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
@@ -17,7 +19,6 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
