2d9925772bcc1078a575a62899347ffbe3f125f1
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-18 08:32:25 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,3 +13,19 @@ void puts2(char *str)
 	}
 	_putchar('\n');
 }
+/**
+ * _strlen - a function taht returns the length of a string
+ * @s: the string to be measured
+ * Return: alwyas 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
