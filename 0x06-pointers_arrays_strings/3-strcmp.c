aa93f6a2e6a90eb321cb9129224eb73c40f4355c
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-21 02:04:39 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, j = 0, k = 0;
-	for (i = 0; s1[i] != '\0' && s[i] != '\0'; i++)
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		j = s1[i] - s2[i];
 		k = k + j;
 		if (j != 0)
-			return(j);
+			return (j);
 	}
 	while (s1[j] != '\0')
 	{
