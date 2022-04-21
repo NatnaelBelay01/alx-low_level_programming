cac9bfa1fadd43824e294c8ce4aa83dceed8032b
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-21 10:36:12 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - a function that is of little use if u ask me !
+ * @s: what ever the checker decides to put in
+ * Return: i dont even wanna talk about it
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char d = "AaEeOoTtLl";
+	char b = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (s[i] == d[j])
+			{
+				s[i] = b[j / 2];
+			}
+		}
+	}
+	return (s);
+}
