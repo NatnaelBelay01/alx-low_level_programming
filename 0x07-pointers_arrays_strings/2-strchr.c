8db6e62b704431051c981cc322f356f5e03b31af
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-25 02:36:09 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - returns the first occurrence of  the character c
+ * @s: the string the be searched from
+ * @c: the desired character
+ * Return: a pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	char *res;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			for (int j = 0; s[j] != '\0'; j++)
+			{
+				res[j] = s[i];
+				i++;
+			}
+			return (res);
+		}
+	}
+	return (NULL);
+}
