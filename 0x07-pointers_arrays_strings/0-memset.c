4827a2f7539d1f826600e67f19a5b63e12a509b5
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-25 01:14:05 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - a function that fills the first n bytes with a constn byte b
+ * @b: the constant charcter or byte
+ * @n: the fist n elements
+ * @s: the array or pointer
+ * Return: s which is the pointer or the array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return s;
+}
