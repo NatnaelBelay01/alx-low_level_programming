a8f6b381fa566d1940f332bb3583f2f739bf2d27
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-25 12:39:23 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chess board
+ * @a: the pieces
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		if(i)
+			_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	}
+}
