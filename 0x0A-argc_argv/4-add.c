ae9a7b4db85652af5ac292156c79ac9eb98e8914
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-29 11:25:35 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * @argc: the size of argv
+ * @argv: the array of arguments
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, sum = 0;
+
+	while (i < argc)
+	{
+		if (!(atoi(argv[i])))
+		{
+			printf("Error");
+			putchar('\n');
+			return (1);
+		}
+		i++;
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d", sum);
+	putchar('\n');
+	return (0);
+}
