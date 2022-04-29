e09970e0760dc107a55ecaa352ffb6f423cad079
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-29 04:08:00 -0700

    Task 1

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies a string
+ * @dest: the destination
+ * @src: the source
+ * @n: bytes
+ * Return: copied file
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
