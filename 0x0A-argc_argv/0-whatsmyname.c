4478f06be0b4704a670e2ad179a5a663ad3eb7bc
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-29 09:57:26 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ * @argc: the size of @argv
+ * @argv: the array of size @argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return 0;
+}
