45b2d135d9ee6dd69a72aecc9b71faf09b4be2ae
Natnael <natnaelbelayteklemariam@gmail.com>
2022-04-29 11:00:02 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * @argc: the size of argv
+ * @argv: an array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, prod = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			prod = prod * atoi(argv[i]);
+		}
+		printf("%d", prod);
+	}
+	else
+		printf("Error");
+	putchar('\n');
+	return (0);
+}
