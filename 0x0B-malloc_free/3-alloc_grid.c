26d83381012092722d69a96fc9a9c52b7a216d73
Natnael <natnaelbelayteklemariam@gmail.com>
2022-05-03 10:56:04 -0700

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - a function that creates a multi-dimensional array
+ * @width: the width of the array
+ * @height: the height of the array
+ * Return: a pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+	int j, k;
+	int **i;
+
+	if (width > 0 && height > 0)
+	{
+		i = malloc(height * sizeof(int *));
+		if (i)
+		{
+			for (j = 0; j < height; j++)
+			{
+				i[j] = malloc(width * sizeof(int *));
+			}
+			for (j = 0; j < height; j++)
+			{
+				for (k = 0; k < width; k++)
+				{
+					i[j][k] = 0;
+				}
+			}
+			return (i);
+		}
+		return ('\0');
+	}
+	return('\0');
+}
