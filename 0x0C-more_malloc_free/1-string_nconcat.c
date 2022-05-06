5568b90dd8cc481e403ebe204884acf205ba0c3d
Natnael <natnaelbelayteklemariam@gmail.com>
2022-05-06 04:51:38 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL || n == 0)
 		return (s1);
 	s = malloc(sizeof(*s1) + n);
+	if (s1 = NULL)
+	{
+		for (i = 0; i < n && s2[i] != '\0'; i++)
+		{
+			s[i] = s2[i];
+		}
+		s[i] = '\0';
+		return (s);
+	}
 	if (s != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 		{
 			s[i] = s1[i];
 		}
-		for (j = 0; j < n && s[j] != '\0'; j++)
+		for (j = 0; j < n && s2[j] != '\0'; j++)
 		{
 			s[i] = s2[j];
 			i++;
