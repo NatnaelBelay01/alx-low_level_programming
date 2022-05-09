aabcd1e13cb3e1bc92f4683b5e3bfab1eae3f55c
Natnael <natnaelbelayteklemariam@gmail.com>
2022-05-09 11:03:16 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initializes the elements of a structure
+ * @d: the variable of type struct dog
+ * @name: the name element
+ * @age: the age element
+ * @owner: the owner element
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
