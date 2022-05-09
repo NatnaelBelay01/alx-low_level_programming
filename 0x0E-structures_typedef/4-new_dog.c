776ed5631a141350bf076946faf6601872d14584
Natnael <natnaelbelayteklemariam@gmail.com>
2022-05-09 15:30:00 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,39 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdg;
+	int i;
 
 	newdg = malloc(sizeof(dog_t));
 	if (newdg == NULL)
 		return (NULL);
-	newdg->name = name;
+	newdg->name = malloc(sizeof(name));
+	if (newdg->name == NULL)
+	{
+		free (newdg->name);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; name[i] != '\0'; i++)
+		{
+			newdg->name[i] = name[i];
+		}
+		newdg->name[i] = '\0';
+	}
 	newdg->age = age;
-	newdg->owner = owner;
+	newdg->owner = malloc(sizeof(owner));
+	if (newdg->owner == NULL)
+	{
+		free(newdg->owner);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; owner[i] != '\0'; i++)
+		{
+			newdg->owner[i] = owner[i];
+		}
+		newdg->owner[i] = '\0';
+	}
 	return (newdg);
 }
