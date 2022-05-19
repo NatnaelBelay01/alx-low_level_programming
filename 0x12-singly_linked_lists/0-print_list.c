bb1db9efac14361ca91d30fb51f92f3a2850fed1
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-19 08:16:08 -0700

    Task 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nill)\n");
 		elem++;
