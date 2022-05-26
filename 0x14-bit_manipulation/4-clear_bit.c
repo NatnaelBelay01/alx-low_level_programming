d8c313656854528f618882ffdcd5f76e7db5a896
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-26 13:37:02 -0700

    Task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,10 +6,10 @@
  * @index: the index
  * Return: 1 or -1
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 32)
 		return (-1);
-	*n = *n & (~(1 << index));
+	*n = *n & ~(1 << index);
 	return (1);
 }
