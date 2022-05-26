ce45d236619589e2d33f7a1f0dc68399ea2a08e8
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-26 13:23:16 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - a function that sets a bit at a certian index
+ * @n: a pointer to the number
+ * @index: the index
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}
