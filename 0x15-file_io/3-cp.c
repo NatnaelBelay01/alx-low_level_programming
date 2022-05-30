86b281d3adf85c790302ef4c5332e689d2d6205a
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-30 12:52:01 -0700

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * main - the main function
+ * @argc: the count of arguments
+ * @argv: array of pointers
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int fdf, fdt, buf_red, buf_rot, clsf, clst;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fdf = open(argv[1], O_RDONLY);
+	buf_red = read(fdf, buf, BUFSIZ);
+	if (fdf < 0 || buf_red < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buf_rot = write(fdt, buf, BUFSIZ);
+	if (fdt < 0 || buf_rot < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
+	clsf = close(fdf);
+	clst = close(fdt);
+	if (clsf < 0 || clst < 0)
+	{
+		if (clsf < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdf);
+		if (clst < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdt);
+		exit(100);
+	}
+	return (0);
+}
