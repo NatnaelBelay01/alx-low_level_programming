99669edfefdb6e0e0b486c8bf5f36e4f4141ee96
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-30 13:18:12 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 {
 	int fdf, fdt, buf_red, buf_rot, clsf, clst;
 	char buf[BUFSIZ];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -20,14 +19,14 @@ int main(int argc, char *argv[])
 	buf_red = read(fdf, buf, BUFSIZ);
 	if (fdf < 0 || buf_red < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	buf_rot = write(fdt, buf, buf_red);
 	if (fdt < 0 || buf_rot < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	clsf = close(fdf);
