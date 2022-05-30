486fe57ea0152558f13d47a26217cc066a22b523
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-05-30 13:07:03 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 {
 	int fdf, fdt, buf_red, buf_rot, clsf, clst;
 	char buf[BUFSIZ];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -24,7 +23,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	buf_rot = write(fdt, buf, BUFSIZ);
+	buf_rot = write(fdt, buf, buf_red);
 	if (fdt < 0 || buf_rot < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
