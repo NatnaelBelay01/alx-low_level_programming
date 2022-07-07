f921e1e9710978694762d718c7963bc13f9a83b6
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-07-07 07:20:28 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,58 +1,54 @@
 #include "lists.h"
 
 /**
- * len - a function that returns the maximum index in a list
- * @head: a pointer to the head
- * Return: the maximum index
- */
-unsigned int len(dlistint_t *head)
-{
-	unsigned int count = 0;
-	dlistint_t *temp = head;
-
-	while (temp->next != NULL && temp != NULL)
-	{
-		temp = temp->next;
-		count++;
-	}
-	return (count);
-}
-/**
- * insert_dnodeint_at_index - a function that inserts a node at the index
- * @h: a poiner to a pointer that points to the head node
- * @idx: the index of the node
- * @n: the data
- * Return: the address of the new node
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h, *new;
-	unsigned int length = len(*h);
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (temp->prev != NULL)
-	{
-		while (temp->prev != NULL)
-			temp = temp->prev;
-	}
-	if (*h == NULL || length + 1 < idx)
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || !h)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
+	new = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (idx == length + 1)
-		return (add_dnodeint_end(h, n));
-	while (idx > 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		temp = temp->next;
-		idx--;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	new->prev = temp->prev;
-	temp->prev->next = new;
-	temp->prev = new;
-	new->next = temp;
+
 	return (new);
 }
